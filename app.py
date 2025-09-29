28d6af1cca95aaaf8e0ad02c5fe569318be6be17
Thanh Long <thanhlongnguyen296@gmail.com>
2025-09-29 15:55:28 +0700

    add numpy

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import gradio as gr
 import torch
 from torchvision import models, transforms
 from PIL import Image
-
+import io
 # Load model
 model = models.resnet18()
 model.fc = torch.nn.Linear(model.fc.in_features, 2)
@@ -16,6 +16,7 @@ transform = transforms.Compose([
 ])
 
 def predict(img):
+    img = Image.open(io.BytesIO(img)).convert("RGB")
     img = transform(img).unsqueeze(0)
     with torch.no_grad():
         preds = model(img)
